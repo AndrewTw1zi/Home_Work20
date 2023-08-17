8cf93ca5772fb70a09d75b73aca81e395dddc43b
Andrew <alekseevandrew105@gmail.com>
2023-08-17 13:16:08 +0300

    homework20

---
+++ src/Lesson41/Lesson41.java
@@ -0,0 +1,11 @@
+package Lesson41;
+
+public class Lesson41 {
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        Dog dog = new Dog();
+        dog.makeSound();
+        cat.makeSound();
+
+    }
+}
